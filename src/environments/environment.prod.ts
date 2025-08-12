1a1a308a00934830eee89e8ba1e5224eb1299df6
Matheus Alves <matheusalvesantana@hotmail.com>
2025-08-12 14:58:50 -0300

    feat: configuração de ambientes de desenvolvimento e produção

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.vsb-onboard360.com.br/api',
+  apiTimeout: 30000,
+  appName: 'VSB Onboard360',
+  version: '1.0.0'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000/api',
+  apiTimeout: 30000,
+  appName: 'VSB Onboard360',
+  version: '1.0.0'
+};
