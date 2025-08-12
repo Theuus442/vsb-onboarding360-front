8e4f5f837bfc0abeee7c6cfd6b00e902a5795d5f
Matheus Alves <matheusalvesantana@hotmail.com>
2025-08-12 14:59:32 -0300

    feat: componente principal de gestão de documentos

---
+++ src/app/modulos/admin/componentes/gestao-documentos/gestao-documentos.component.ts
@@ -0,0 +1,4 @@
+// Arquivo movido para src/app/modulos/admin/paginas/gestao-documentos/
+// Esta versão foi substituída por uma nova implementação seguindo o padrão das outras telas
+
+export * from '../../paginas/gestao-documentos/gestao-documentos.component';
